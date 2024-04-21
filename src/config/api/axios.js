9a7af0dbea3d728dc5a57db4fc75d667346e783d
aryankaushik11 <aryankaushik105@gmail.com>
2024-04-22 01:47:13 +0530

    Error correction in register

--- src/config/api/axios.js
+++ src/config/api/axios.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL:
-    
-     "http://localhost:3000",
+  baseURL:    
+     "http://localhost:3500",
 
   headers: { "Content-Type": "application/json" },
 });
